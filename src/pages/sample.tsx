15755ea9691abf20652f69649daca0ff571b99ff
IT-imataka <takashi6128_gakky@outlook.com>
2025-01-05 21:38:56 +0900

    ホスティングテスト

---
+++ src/pages/sample.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/globals.css'
+
+export default function Home(){
+  return (
+    <div>
+      <iframe className={styles.main} src="portfolio/index.html"/>
+    </div>
+  )
+}
